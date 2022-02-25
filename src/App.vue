05817a618aa13462cfa5cdffd90d5fe6ebf9de8d
orlandollerena <trancetribal@bellsouth.net>
2022-02-24 22:26:32 -0500

    fresh start

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template lang="pug">
+  #app
+
+    a(
+      v-if="quote.link"
+      :href="quote.link"
+    ) {{ quote.body }}
+
+    span(v-else) {{ quote.body }}
+    <!--<PwaNotification />-->
+</template>
+
+<script>
+  import PwaNotification from './components/PwaNotification.vue'
+  import { getRandomQuote } from '@/quotes'
+
+  export default {
+    name: 'app',
+    data() {
+      return {
+        quote: null,
+      }
+    },
+    components: {
+      PwaNotification,
+    },
+    mounted() {
+      this.quote = getRandomQuote()
+    },
+  }
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
